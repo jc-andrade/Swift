1f5e307696657ef494c62fe5acc0a78c9ae48924
Julio Andrade <jcamvz@gmail.com>
2016-05-16 22:59:15 -0500

    Tables work

---
+++ Homepwner/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Julio Andrade on 5/16/16.
+//  Copyright © 2016 Julio Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
